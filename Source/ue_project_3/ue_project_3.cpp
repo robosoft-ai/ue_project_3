709219a5c5f6f827eb86649020c62088ef04fcba
pabloinigoblasco <pibgeus@gmail.com>
2023-08-23 12:54:09 -0700

    Initial Clone and rename

---
+++ Source/ue_project_3/ue_project_3.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ue_project_3.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ue_project_3, "ue_project_3" );
